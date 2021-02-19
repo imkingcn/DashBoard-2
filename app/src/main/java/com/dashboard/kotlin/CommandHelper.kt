af37bb835ac2f1a2c7541f25db9bf9d45a8a9b25
Jkkoi <jkkoi@jkkoi.top>
2021-02-19 21:03:56 +0800

    Init

---
+++ app/src/main/java/com/dashboard/kotlin/CommandHelper.kt
@@ -0,0 +1,199 @@
+package com.dashboard.kotlin
+
+import android.R.attr.port
+import android.content.Context
+import android.os.Handler
+import android.util.Log
+import android.widget.Toast
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
+
+
+class CommandHelper(context: Context) {
+    private val context = context
+    private val handler: Handler = Handler()
+    //                handler.post(Runnable {
+//                    Toast.makeText(context.applicationContext,
+//                            "下载完成", Toast.LENGTH_LONG).show()
+//                })
+
+
+    fun saveFile(path: String, filename: String, text: String): Unit {
+        try {
+            val f = File(path, filename)
+            if (!f.exists()){
+                if (!f.getParentFile().exists()){
+                    f.getParentFile().mkdirs()
+                }
+            }
+            f.writeText(text)
+        } catch (e: Exception){Log.e("saveFile", "Exception: $e")}
+
+    }
+
+    fun download(link: String, path: String, filename: String): Boolean {
+        var isSuccess = false
+        val downloadThread: Thread = Thread{
+            Log.i("download", "下载开始")
+            try {
+                    URL(link).openStream().use { input ->
+                        FileOutputStream({
+                            val f = File(path, filename)
+                            try {
+                                if (!f.exists()) {
+                                    if (!f.getParentFile().exists()) {
+                                        f.getParentFile().mkdirs()
+                                    }
+                                }
+
+                            } catch (e: Exception) {
+                                Log.e("download", "Exception: $e")
+                            }
+                            f
+                        }()).use { output ->
+                            input.copyTo(output)
+                        }
+                    }
+
+            isSuccess = true
+
+            }catch (e: Exception) {
+                Log.e("download", "Exception: $e")
+            }
+        }
+
+        downloadThread.start()
+        downloadThread.join()
+
+
+
+
+        handler.post(Runnable {
+                    Toast.makeText(context, {
+                        if (isSuccess) {
+                            context.getString(R.string.downloadSuccess)
+                        } else {
+                            context.getString(R.string.downloadFailure)
+                        }
+                    }(), Toast.LENGTH_SHORT).show()
+                })
+
+        Log.i("download", "下载线程退出")
+
+        return isSuccess
+
+
+
+    }
+
+    fun downloadGithub(link: String, path: String, filename: String, useCDN: Boolean): Boolean{
+        return download({
+            if (useCDN) {
+                context.getString(R.string.CDN) + link
+            } else {
+                link
+            }
+        }(), path, filename)
+    }
+
+    fun suCmd(cmd: String): String {
+        var process: Process? = null
+        var os: DataOutputStream? = null
+        var ls: DataInputStream? = null
+        var result: String = ""
+        Log.d("suCmd", "Command is: $cmd")
+        try {
+            process = Runtime.getRuntime().exec("su")
+            os = DataOutputStream(process.outputStream)
+            ls = DataInputStream(process.inputStream)
+            os.writeBytes(cmd + "\n")
+            os.writeBytes("exit\n")
+            os.flush()
+            var line = ls.readLine()
+            while (line != null) {
+                result += line
+                line = ls.readLine()
+            }
+            process.waitFor()
+        } catch (e: Exception) {
+            Log.e("suCmd", "Exception: $e")
+        } finally {
+            try {
+                os?.close()
+                ls?.close()
+                process?.destroy()
+            } catch (e: Exception) {
+                Log.e("suCmd", "close stream exception: $e")
+            }
+        }
+        Log.d("suCmd", "result: $result")
+        return result
+    }
+
+    fun modifyConfiguration(confFilePath: String){
+        var grepR: String = ""
+        var awkR: String =""
+
+        val replaceMap: Map<String,String> = mapOf(
+                "Proxy" to "proxies",
+                "Proxy Group" to "proxy-groups",
+                "Rule" to "rules",
+                "proxy-provider" to "proxy-providers",
+                "rule-provider" to "rule-providers",
+                "Final" to "Match",
+                "SOURCE-IP-CIDR"  to "SRC-IP-CIDR")
+
+        replaceMap.map { (before, after) ->
+            suCmd("sed -i 's/^${before}/${after}/g' $confFilePath")
+
+        }
+
+
+
+        grepR = suCmd("cat $confFilePath | grep \"external-controller\"")
+        if (grepR == ""){
+            suCmd("sed -i '/^proxies/i external-controller: 127.0.0.1:63001' $confFilePath")
+        }
+        else{
+            awkR = suCmd(" echo \"${grepR}\" | awk -F ' ' '{print \$2}' ")
+            if (awkR == ""){
+                suCmd("sed -i '/^external-controller/c external-controller: 127.0.0.1:63001' $confFilePath")
+            }
+
+        }
+
+
+        grepR = suCmd("cat $confFilePath | grep \"external-ui\"")
+        if (grepR == ""){
+            suCmd("sed -i '/^external-controller/a external-ui: yacd-gh-pages' $confFilePath")
+        }
+        else{
+            suCmd("sed -i '/^external-ui/c external-ui: yacd-gh-pages' $confFilePath")
+        }
+
+
+        grepR = suCmd("cat $confFilePath | grep \"tproxy-port\"")
+        if (grepR == ""){
+            suCmd("sed -i '/^proxies/i tproxy-port: 63002' $confFilePath")
+        }
+        else{
+            awkR = suCmd(" echo \"${grepR}\" | awk -F ':' '{print \$2}' ")
+            if (awkR == ""){
+                suCmd("sed -i '/^tproxy-port/c tproxy-port: 127.0.0.1:63002' $confFilePath")
+            }
+        }
+
+        awkR = suCmd("grep \"listen\" $confFilePath | awk -F ':' '{print \$3}'  ")
+        if (awkR == ""){
+            suCmd("sed -i '/^proxies/i " +
+                    "dns:\\n  enable: true\\n  ipv6: false\\n  listen: 0.0.0.0:1053\\n  enhanced-mode: redir-host \\n  default-nameserver: \\n    - 223.5.5.5 \\n  nameserver:\\n    - tls://dns.pub\\n    - tls://doh.pub\\n    # - tls://223.5.5.5\\n    # - tls://185.222.222.222\\n    - https://doh.pub/dns-query\\n    - https://dns.pub/dns-query\\n    - https://dns.alidns.com/dns-query\\n    - https://rubyfish.cn/dns-query\\n  fallback:\\n    # - tls://1.1.1.1\\n    # - tls://8.8.8.8\\n    - https://cloudflare-dns.com/dns-query\\n    - https://dns.google/dns-query\\n    - https://doh.dns.sb/dns-query\\n    - https://doh.opendns.com/dns-query\\n    - https://doh.pub/dns-query\\n    - https://dns.alidns.com/dns-query' " +
+                    "$confFilePath")
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/dashboard/kotlin/MainActivity.kt
@@ -0,0 +1,290 @@
+package com.dashboard.kotlin
+
+import android.content.*
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.*
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import java.io.File
+
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var cmdHelper:CommandHelper
+    private lateinit var webView: WebView
+    private val handler: Handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AppCenter.start(application, "541c81e0-57ce-4ea1-a6df-503900749e46",
+                Analytics::class.java, Crashes::class.java)
+
+        cmdHelper = CommandHelper(applicationContext)
+
+        if (!File(dataDir,"started").exists()){
+            val dirArray: Array<String> = arrayOf("/data/adb/clash","/sdcard/Documents/clash")
+            for (trydir in dirArray){
+                val suCmdR: String = cmdHelper.suCmd("if [ -d \"${trydir}\" ];then echo \"${trydir}\"; fi")
+                if (suCmdR != "") {
+                    cmdHelper.suCmd("mv $suCmdR "+getString(R.string.ConfigDir))
+                    Toast.makeText(applicationContext,"配置文件已迁移到新目录",Toast.LENGTH_LONG).show()
+                    break
+                }
+            }
+            File(dataDir,"started").createNewFile()
+        }
+
+        val dashBoardURL: String = "http://" + cmdHelper.suCmd(
+                "grep \"external-controller\" " +
+                        getString(R.string.ConfigDir)+getString(R.string.ConfigFile) +
+                        "| awk -F ' ' '{print \$2}'") +
+                "/ui"
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val toolBar:androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        val navigationview: NavigationView = findViewById(R.id.navigation_view)
+        val drawer:DrawerLayout = findViewById(R.id.drawer_layout)
+        setSupportActionBar(toolBar)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolBar, 0, 0
+        )
+        drawer.setDrawerListener(toggle)
+        toggle.syncState()
+
+        val headerView = navigationview.getHeaderView(0)
+
+
+        //<editor-fold desc="按键监听事件区">
+        //<editor-fold desc="切换版本 btn">
+        val switchClashVersionBtn: Button = navigationview.findViewById(R.id.footer_item_switchClashVersionBtn)
+        switchClashVersionBtn.setOnClickListener{
+
+            Toast.makeText(applicationContext,"已废弃",Toast.LENGTH_SHORT).show()
+
+        }
+        //</editor-fold>
+
+
+        val selfReporBtn: Button = navigationview.findViewById(R.id.footer_item_selfReportBtn)
+        selfReporBtn.setOnClickListener{
+//            Toast.makeText(applicationContext,"已将你的 操作记录/节点信息/IP信息 提交到公安部在线举报中心",Toast.LENGTH_LONG).show()
+//            Toast.makeText(applicationContext,"望改邪归正",Toast.LENGTH_LONG).show()
+//            Toast.makeText(applicationContext,"FAKE MESSAGE",Toast.LENGTH_LONG).show()
+        }
+
+        //<editor-fold desc="头像点击事件">
+        val imageView: ImageView = headerView.findViewById(R.id.iv_head)
+        imageView.setOnClickListener {
+            Toast.makeText(applicationContext, "新年快乐", Toast.LENGTH_SHORT).show()
+        }
+        //</editor-fold>
+
+        //设置条目点击监听
+        //<editor-fold desc="侧栏点击事件">
+        navigationview.setNavigationItemSelectedListener { menuItem ->
+            Log.i("setNavigationItemSelectedListener",menuItem.itemId.toString())
+            when(menuItem.itemId){
+                R.id.subscriptionManagementItem -> {
+                    val editText = EditText(this)
+                    val inputDialog = AlertDialog.Builder(this)
+                    inputDialog.setTitle("还没写")
+                        .setView(editText)
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                        DialogInterface.OnClickListener { dialog, which ->
+
+                        }).show()
+
+                }
+                R.id.checkIPItem -> webView.loadUrl("https://ip.skk.moe/")
+                R.id.dashBoardItem -> {
+                    webView.loadUrl(dashBoardURL)
+                }
+                R.id.updateGeoIPDatabaseItem -> {
+                    Toast.makeText(applicationContext,"GeoIP 开始下载",Toast.LENGTH_SHORT).show()
+                    Thread{
+
+                        val isSuccess: Boolean = cmdHelper.downloadGithub(getString(R.string.GeoIPURL),
+                                externalCacheDir.toString(),
+                                getString(R.string.GeoIPDatabaseFile),
+                                true)
+                        if (isSuccess){
+                            val oldDir: String =externalCacheDir.toString()+
+                                    getString(R.string.GeoIPDatabaseFile)
+                            val newDir: String =getString(R.string.ConfigDir) +
+                                    getString(R.string.GeoIPDatabaseFile)
+                            cmdHelper.suCmd("mv $oldDir $newDir")
+
+                            handler.post(Runnable {
+                                Toast.makeText(applicationContext,
+                                        "更新完成", Toast.LENGTH_LONG).show()
+                            })
+                        }
+
+
+                    }.start()
+
+                }
+                R.id.downLoadYACDItem ->{
+                    Toast.makeText(applicationContext,"YACD 面板 开始下载",Toast.LENGTH_SHORT).show()
+
+                    Thread{
+
+                        val isSuccess: Boolean = cmdHelper.downloadGithub(
+                                getString(R.string.yacdURL),
+                                externalCacheDir.toString(),
+                                "yacd.zip",
+                                true
+                        )
+                        if (isSuccess) {
+                            cmdHelper.suCmd("unzip ${externalCacheDir}/yacd.zip -d " +
+                                    getString(R.string.ConfigDir))
+
+                            cmdHelper.modifyConfiguration(getString(R.string.ConfigDir)+
+                                    getString(R.string.ConfigFile))
+
+
+                        }
+
+                        handler.post(Runnable {
+                            Toast.makeText(applicationContext,
+                            "安装完成", Toast.LENGTH_LONG).show()
+                        })
+                    }.start()
+
+
+                }
+                R.id.modifyConfigurationItem ->{
+                    Toast.makeText(applicationContext,"正在修改",Toast.LENGTH_SHORT).show()
+
+                    Thread{
+                        cmdHelper.modifyConfiguration(getString(R.string.ConfigDir)+
+                                getString(R.string.ConfigFile))
+
+                        handler.post(Runnable {
+                            Toast.makeText(applicationContext,
+                                    "修改完成", Toast.LENGTH_LONG).show()
+                        })
+                    }.start()
+
+                }
+                else -> Toast.makeText(applicationContext,
+                        menuItem.title.toString() + "-- 还没做",
+                        Toast.LENGTH_SHORT).show()
+            }
+
+            //设置哪个按钮被选中
+//                menuItem.setChecked(true);
+            //关闭侧边栏
+            drawer.closeDrawers();
+            false
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        //<editor-fold desc="WebView">
+        // WebView
+        Log.i("URL",dashBoardURL)
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.databaseEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.saveFormData = true
+        webView.webViewClient = MyWebViewClient()
+        webView.loadUrl(dashBoardURL)
+        //</editor-fold>
+
+
+
+
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()){
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    class MyWebViewClient: WebViewClient(){
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return true
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.control_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(applicationContext,"开始执行命令",Toast.LENGTH_SHORT).show()
+        when (item.itemId) {
+            R.id.startClash -> cmdHelper.suCmd(getString(R.string.startClashCmd))
+            R.id.stopClash -> cmdHelper.suCmd(getString(R.string.stopClashCmd))
+            R.id.restartClash -> cmdHelper.suCmd(getString(R.string.restartClashCmd))
+            else ->return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+    private fun showInputPasswordDialog() {
+
+    }
+
+}
+
+
+//<editor-fold desc="ContentProvider - Disable">
+//class InitProvider : ContentProvider() {
+//
+//    override fun onCreate(): Boolean {
+//        // 这里可以获得 context 属性
+//        return false
+//    }
+//
+//    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+//
+//    override fun query(
+//            uri: Uri,
+//            projection: Array<out String>?,
+//            selection: String?,
+//            selectionArgs: Array<out String>?,
+//            sortOrder: String?
+//    ): Cursor? = null
+//
+//    override fun update(
+//            uri: Uri,
+//            values: ContentValues?,
+//            selection: String?,
+//            selectionArgs: Array<out String>?
+//    ): Int = 0
+//
+//    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int = 0
+//
+//    override fun getType(uri: Uri): String? = null
+//}
+//</editor-fold>
